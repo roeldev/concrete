05637221409bdcbef60cfd6f0adcdfe11d75e8ee
Roel Dev <roeldev@users.noreply.github.com>
2015-07-22 16:30:41 +0200

    Dependencies and gulpfile updates

--- Gemfile
+++ Gemfile
@@ -1,3 +1,2 @@
 source 'https://rubygems.org'
-gem 'compass', '~> 1.0.3'
 gem 'scss_lint', '~> 0.38.0'
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 /**
  * concrete | gulpfile.js
- * file version: 0.00.004
  */
 'use strict';
 
@@ -12,17 +11,22 @@ var GulpSass            = require('gulp-sass');
 var GulpScssLint        = require('gulp-scss-lint');
 var GulpScssLintStylish = require('gulp-scss-lint-stylish');
 var GulpSize            = require('gulp-size');
-var Confirge            = require('confirge');
-var Path                = require('path');
-var RunSequence         = require('run-sequence');
 var SassDoc             = require('sassdoc');
 
 ////////////////////////////////////////////////////////////////////////////////
 
 var SRC_EXAMPLES = './examples/scss/*.scss';
-var SRC_TESTS    = './tests/*.scss';
-var SRC_SOURCE   = ['./source/**/*.scss', './*.scss',
-                    '!node_modules/*', '!sassdoc/*'];
+var SRC_TESTS    = './test/*.scss';
+var SRC_SOURCE   = ['./src/**/*.scss', './*.scss',
+                    '!node_modules/*', '!docs/*'];
+
+//------------------------------------------------------------------------------
+
+var $sassOptions =
+{
+    'style':         'expanded',
+    'sourceComments': true
+};
 
 var $plumberOptions =
 {
@@ -35,30 +39,10 @@ var $plumberOptions =
 
 //------------------------------------------------------------------------------
 
-function compileTask($src, $dest)
+// delay task so it won't fail when the editor has locked the file while saving
+function GulpDelayTask($delay, $fn)
 {
-    var $sassOptions =
-    {
-        'style':         'expanded',
-        'sourceComments': true
-    };
-
-    return function()
-    {
-        return Gulp.src($src)
-            .pipe( GulpPlumber($plumberOptions) )
-            .pipe( GulpSass($sassOptions) )
-            .pipe( GulpSize({ 'showFiles': true }) )
-            .pipe( Gulp.dest($dest) );
-    };
-}
-
-function watchTask($src, $task)
-{
-    return function()
-    {
-        return Gulp.watch([].concat(SRC_SOURCE, [$src]), $task);
-    };
+    setTimeout($fn, $delay);
 }
 
 //------------------------------------------------------------------------------
@@ -66,31 +50,45 @@ function watchTask($src, $task)
 /*
     Examples related tasks
  */
-Gulp.task('compile:examples', compileTask(SRC_EXAMPLES, 'examples/css'));
-Gulp.task('watch:examples',   watchTask(SRC_EXAMPLES, 'compile:examples'));
-
-/*
-    Tests related tasks
- */
-Gulp.task('compile:tests', compileTask(SRC_TESTS, 'tests/css'));
-Gulp.task('watch:tests',   watchTask(SRC_TESTS, 'compile:tests'));
-
-/*
-    Sass linter
- */
-Gulp.task('lint', function()
+Gulp.task('compile:examples', function($callback)
 {
-    //var $config  = Confirge.read('./.scss-lint.yml');
-    var $src     = [].concat(SRC_SOURCE, [SRC_EXAMPLES]);
-    var $options =
+    GulpDelayTask(500, function()
     {
-        'bundleExec':   true,
-        'verbose':      true,
-        'customReport': GulpScssLintStylish
-    };
+        Gulp.src(SRC_EXAMPLES)
+            .pipe( GulpPlumber($plumberOptions) )
+            .pipe( GulpSass($sassOptions) )
+            .pipe( GulpSize({ 'showFiles': true }) )
+            .pipe( Gulp.dest('examples/css') );
 
-    return Gulp.src(SRC_SOURCE)
-        .pipe( GulpScssLint($options) );
+        $callback();
+    });
+});
+
+Gulp.task('watch:examples', function()
+{
+    var $src = [].concat(SRC_SOURCE, [SRC_EXAMPLES]);
+    Gulp.watch($src, ['compile:examples']);
+});
+
+/*
+    Runs True Sass test suite with Mocha.
+ */
+Gulp.task('test', function($callback)
+{
+    GulpDelayTask(500, function()
+    {
+        Gulp.src('test/*.js', { 'read': false })
+            .pipe( GulpPlumber($plumberOptions) )
+            .pipe( GulpMocha({ 'reporter': 'spec' }) );
+
+        $callback();
+    });
+});
+
+Gulp.task('watch:tests', function()
+{
+    var $src = [].concat(SRC_SOURCE, [SRC_TESTS]);
+    Gulp.watch($src, ['test']);
 });
 
 /*
@@ -100,35 +98,13 @@ Gulp.task('watch', function()
 {
     var $src = [].concat(SRC_SOURCE, [SRC_EXAMPLES, SRC_TESTS]);
     Gulp.watch($src, ['watch:examples', 'watch:tests']);
-    Gulp.watch(SRC_SOURCE, ['lint'])
-});
-
-Gulp.task('test', function()
-{
-    setTimeout(function()
-    {
-        Gulp.src('test/*.js', { 'read': false })
-            .pipe( GulpPlumber($plumberOptions) )
-            .pipe( GulpMocha({ 'reporter': 'spec' }) );
-    }, 500);
-});
-
-Gulp.task('dev', function()
-{
-    process.stdout.write('\u001b[2J');
-    RunSequence('test', 'lint');
-});
-
-Gulp.task('watch:dev', function()
-{
-    Gulp.watch(['source/**/*.scss', 'test/*.scss'], ['test']);
+    //Gulp.watch(SRC_SOURCE, ['lint'])
 });
 
 /*
     Builder, run before creating new release
  */
-Gulp.task('build', ['compile:examples', 'compile:tests'], function()
+Gulp.task('build', ['compile:examples'], function()
 {
-    Gulp.src(SRC_SOURCE)
-        .pipe( SassDoc() )
+    SassDoc('./src/');
 });
