2cd79dc06abf0b07724afcfed2236f550357537f
Roel Dev <roeldev@users.noreply.github.com>
2016-10-29 12:56:49 +0200

    Updated task runner for tests and linting

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,30 @@ const SassDoc   = require('sassdoc');
 
 Maelstrom.init(Gulp, ['maelstrom-sass']);
 
-// -----------------------------------------------------------------------------
+const SRC_FILES  = Maelstrom.sass.src();
+const TEST_FILES = Maelstrom.sass.src('test/**/*.scss');
 
-Gulp.task('sass:doc', function()
+// // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+Gulp.task('sass:doc', () =>
 {
     SassDoc('./src/');
+});
+
+Gulp.task('watch', () =>
+{
+    Gulp.watch(SRC_FILES, ['sass']);
+    Gulp.watch(TEST_FILES, ['sass:lint', 'sass:test']);
+});
+
+Gulp.task('watch:test', () =>
+{
+    // Gulp.watch(SRC_FILES, ['sass']);
+    Gulp.watch(TEST_FILES, ['sass:test']);
+});
+
+Gulp.task('watch:lint', () =>
+{
+    Gulp.watch(SRC_FILES, ['sass']);
+    Gulp.watch(TEST_FILES, ['sass:lint']);
 });
---
+++ test/main.js
@@ -0,0 +1,9 @@
+/**
+ * concrete | test/main.js
+ */
+'use strict';
+
+const Maelstrom    = require('Maelstrom');
+const executeTests = require('../node_modules/maelstrom-sass/lib/utils/executeTests.js');
+
+executeTests(__dirname);
